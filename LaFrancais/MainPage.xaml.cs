50c16c95e2d397d9ed0a40827cb728dcd17d2a18
dkinvoker <dkinvoker@gmail.com>
2023-10-08 23:39:35 +0200

    Uga

--- LaFrancais/MainPage.xaml.cs
+++ LaFrancais/MainPage.xaml.cs
@@ -5,10 +5,13 @@ namespace LaFrancais
     public partial class MainPage : ContentPage
     {
         private string EntriesCount => $"{QuizManager.UsedCount} / {QuizManager.Count}";
+        private QuizEntry CurrentEntry { get; set; }
 
         public MainPage()
         {
             InitializeComponent();
+            this.Loading_ProgressBar.ProgressTo(1, 3000, Easing.CubicIn);
+            this.InitialLoad();
         }
 
         private void Button_Clicked(object sender, EventArgs e)
@@ -19,8 +22,31 @@ namespace LaFrancais
 
         private async void Confirm_button_Clicked(object sender, EventArgs e)
         {
-            this.Meaning_Label.Text = (await QuizManager.GetNextEntry())!.Meaning;
+            if (this.Input_Editor.Text.ToLower() == this.CurrentEntry.FrancaisSpelling.ToLower())
+            {
+                await DisplayAlert("Très bien!", $"'{this.CurrentEntry.FrancaisSpelling}' == '{this.CurrentEntry.Meaning}'", "suivant");
+
+                this.CurrentEntry = (await QuizManager.GetNextEntry())!;
+                this.Meaning_Label.Text = this.CurrentEntry.Meaning;
+                this.Count_label.Text = this.EntriesCount;
+                this.Image_Image.Source = this.CurrentEntry.ImageLink;
+            }
+            else
+            {
+                await DisplayAlert("Mal!", $"'{this.CurrentEntry.FrancaisSpelling}' == '{this.CurrentEntry.Meaning}'", "suivant");
+            }  
+
+            this.Input_Editor.Focus();
+        }
+
+        private async Task InitialLoad()
+        {
+            this.CurrentEntry = (await QuizManager.GetNextEntry())!;
+            this.Meaning_Label.Text = this.CurrentEntry.Meaning;
             this.Count_label.Text = this.EntriesCount;
+            this.Image_Image.Source = this.CurrentEntry.ImageLink;
+
+            this.Loading_Grid.IsVisible = false;
         }
     }
 }
